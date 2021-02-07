13fce6fad3130f3de0c14bab8450e4bcdf0c4fe4
André Neves <andreneves3@gmail.com>
2021-02-07 20:38:10 -0300

    feat: include first project

---
+++ public/projetos/fase-01/basic.js
@@ -0,0 +1,248 @@
+$(function () {
+  iniciaWebGL();
+});
+
+function iniciaWebGL() {
+  var canvas = $("#canvas-webgl")[0];
+  iniciarGL(canvas);
+  iniciarShaders();
+
+  document.dispatchEvent(new Event("afterPrepareWebGl"));
+}
+
+function iniciarGL(canvas) {
+  mMatrix = mat4.create();
+  vMatrix = mat4.create();
+  pMatrix = mat4.create();
+
+  try {
+    gl = canvas.getContext("webgl") || canvas.getContext("experimental-webgl");
+    gl.viewportWidth = canvas.width;
+    gl.viewportHeight = canvas.height;
+  } catch (e) {
+    if (!gl) alert("Não pode inicializar WebGL, desculpe");
+  }
+}
+
+function iniciarShaders() {
+  var vertexShader = getShader(gl, "#shader-vs");
+  var fragmentShader = getShader(gl, "#shader-fs");
+
+  shaderProgram = gl.createProgram();
+  gl.attachShader(shaderProgram, vertexShader);
+  gl.attachShader(shaderProgram, fragmentShader);
+  gl.linkProgram(shaderProgram);
+
+  if (!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)) {
+    alert("Não pode inicializar shaders");
+  }
+
+  gl.useProgram(shaderProgram);
+
+  shaderProgram.vertexPositionAttribute = gl.vertexPositionAttribute = gl.getAttribLocation(
+    shaderProgram,
+    "aVertexPosition"
+  );
+  gl.enableVertexAttribArray(shaderProgram.vertexPositionAttribute);
+
+  // Cores
+  shaderProgram.vertexColorAttribute = gl.getAttribLocation(
+    shaderProgram,
+    "aVertexColor"
+  );
+
+  if (shaderProgram.vertexColorAttribute != -1) {
+    gl.enableVertexAttribArray(shaderProgram.vertexColorAttribute);
+  }
+
+  // Texturas
+  shaderProgram.vertexTextureCoordAttribute = gl.getAttribLocation(
+    shaderProgram,
+    "aTextureCoord"
+  );
+  if (shaderProgram.vertexTextureCoordAttribute != -1) {
+    gl.enableVertexAttribArray(shaderProgram.vertexTextureCoordAttribute);
+  }
+
+  shaderProgram.pMatrixUniform = gl.getUniformLocation(
+    shaderProgram,
+    "uPMatrix"
+  );
+  shaderProgram.vMatrixUniform = gl.getUniformLocation(
+    shaderProgram,
+    "uVMatrix"
+  );
+  shaderProgram.mMatrixUniform = gl.getUniformLocation(
+    shaderProgram,
+    "uMMatrix"
+  );
+}
+
+function getShader(gl, id) {
+  var shaderScript = $(id)[0];
+  if (!shaderScript) {
+    return null;
+  }
+
+  var str = "";
+  var k = shaderScript.firstChild;
+  while (k) {
+    if (k.nodeType == 3) str += k.textContent;
+    k = k.nextSibling;
+  }
+
+  var shader;
+  if (shaderScript.type == "x-shader/x-fragment") {
+    shader = gl.createShader(gl.FRAGMENT_SHADER);
+  } else if (shaderScript.type == "x-shader/x-vertex") {
+    shader = gl.createShader(gl.VERTEX_SHADER);
+  } else {
+    return null;
+  }
+
+  gl.shaderSource(shader, str);
+  gl.compileShader(shader);
+
+  if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+    alert(gl.getShaderInfoLog(shader));
+    return null;
+  }
+
+  return shader;
+}
+
+// eslint-disable-next-line no-unused-vars
+function createBuffer(vertexMatrix) {
+  const buffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+
+  gl.bufferData(
+    gl.ARRAY_BUFFER,
+    new Float32Array(vertexMatrix.flat()),
+    gl.STATIC_DRAW
+  );
+
+  buffer.numItems = vertexMatrix.length;
+  buffer.itemSize = vertexMatrix[0].length;
+
+  return buffer;
+}
+
+// eslint-disable-next-line no-unused-vars
+function createIndexBuffer(vertexMatrix) {
+  const buffer = gl.createBuffer();
+  gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, buffer);
+
+  gl.bufferData(
+    gl.ELEMENT_ARRAY_BUFFER,
+    new Uint16Array(vertexMatrix.flat()),
+    gl.STATIC_DRAW
+  );
+
+  buffer.numItems = vertexMatrix.length;
+  buffer.itemSize = vertexMatrix[0].length;
+
+  return buffer;
+}
+
+// eslint-disable-next-line no-unused-vars
+function prepareScene() {
+  gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+  mat4.perspective(
+    pMatrix,
+    45,
+    gl.viewportWidth / gl.viewportHeight,
+    0.1,
+    100.0,
+    pMatrix
+  );
+
+  mat4.identity(mMatrix);
+  mat4.identity(vMatrix);
+}
+
+// eslint-disable-next-line no-unused-vars
+function drawBufferObject(
+  positionBufferObject,
+  colorBufferObject,
+  indexBufferObject = null,
+  mode = gl.TRIANGLE_STRIP,
+  textureBufferObject = null,
+  texture
+) {
+  gl.bindBuffer(gl.ARRAY_BUFFER, positionBufferObject);
+  gl.vertexAttribPointer(
+    shaderProgram.vertexPositionAttribute,
+    positionBufferObject.itemSize,
+    gl.FLOAT,
+    false,
+    0,
+    0
+  );
+
+  if (!!colorBufferObject) {
+    gl.bindBuffer(gl.ARRAY_BUFFER, colorBufferObject);
+    gl.vertexAttribPointer(
+      shaderProgram.vertexColorAttribute,
+      colorBufferObject.itemSize,
+      gl.FLOAT,
+      false,
+      0,
+      0
+    );
+  }
+
+  if (!!textureBufferObject) {
+    gl.bindBuffer(gl.ARRAY_BUFFER, textureBufferObject);
+    gl.vertexAttribPointer(
+      shaderProgram.vertexTextureCoordAttribute,
+      textureBufferObject.itemSize,
+      gl.FLOAT,
+      false,
+      0,
+      0
+    );
+
+    if (!!texture) {
+      gl.activeTexture(gl.TEXTURE0);
+      gl.bindTexture(gl.TEXTURE_2D, texture);
+      gl.uniform1i(shaderProgram.samplerUniform, 0);
+    }
+  }
+
+  setMatrixUniforms();
+
+  if (!!indexBufferObject) {
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBufferObject);
+    gl.drawElements(mode, indexBufferObject.numItems, gl.UNSIGNED_SHORT, 0);
+    return;
+  }
+
+  gl.drawArrays(mode, 0, positionBufferObject.numItems);
+}
+
+function setMatrixUniforms() {
+  gl.uniformMatrix4fv(shaderProgram.pMatrixUniform, false, pMatrix);
+  gl.uniformMatrix4fv(shaderProgram.vMatrixUniform, false, vMatrix);
+  gl.uniformMatrix4fv(shaderProgram.mMatrixUniform, false, mMatrix);
+}
+
+let mMatrixPilha = [];
+// eslint-disable-next-line no-unused-vars
+function mPushMatrix() {
+  var copy = mat4.clone(mMatrix);
+  mMatrixPilha.push(copy);
+}
+
+// eslint-disable-next-line no-unused-vars
+function mPopMatrix() {
+  if (mMatrixPilha.length == 0) {
+    throw "inválido popMatrix!";
+  }
+  mMatrix = mMatrixPilha.pop();
+}
+
+// eslint-disable-next-line no-unused-vars
+function degToRad(graus) {
+  return (graus * Math.PI) / 180;
+}
---
+++ public/projetos/fase-01/script.js
@@ -0,0 +1,188 @@
+let cuboVertexPositionBuffer;
+let cuboVertexIndexBuffer;
+var cuboVertexTextureCoordBuffer;
+
+var texturas = Array();
+
+var xRot = 0;
+var xVelo = 0;
+
+var yRot = 0;
+var yVelo = 0;
+
+var z = -5;
+
+var teclasPressionadas = {};
+
+document.addEventListener("afterPrepareWebGl", () => {
+  iniciarAmbiente();
+  createCubo();
+  iniciarTextura();
+
+  document.onkeydown = eventoTeclaPress;
+  document.onkeyup = eventoTeclaSolta;
+
+  tick();
+});
+
+function iniciarAmbiente() {
+  gl.clearColor(0.0, 0.0, 0.0, 1.0);
+  gl.enable(gl.DEPTH_TEST);
+}
+
+function tick() {
+  requestAnimFrame(tick);
+  prepareScene();
+  tratarTeclado();
+  desenharCena();
+  animate();
+}
+
+function tratarTeclado() {
+  if (teclasPressionadas[33]) {
+    // Page Up
+    z += 0.05;
+  }
+  if (teclasPressionadas[34]) {
+    // Page Down
+    z -= 0.05;
+  }
+  if (teclasPressionadas[37]) {
+    // Esquerda
+    if (yVelo >= 1) {
+      yVelo -= 1;
+    }
+  }
+  if (teclasPressionadas[39]) {
+    // Direita
+    yVelo += 1;
+  }
+  if (teclasPressionadas[38]) {
+    // Cima
+    xVelo += 1;
+  }
+  if (teclasPressionadas[40]) {
+    // Baixo
+    if (xVelo >= 1) {
+      xVelo -= 1;
+    }
+  }
+}
+
+function desenharCena() {
+  mat4.translate(mMatrix, mMatrix, [0.0, 0.0, z]);
+  desenharCubo();
+}
+
+function desenharCubo() {
+  mat4.rotate(mMatrix, mMatrix, degToRad(xRot), [1, 0, 0]);
+  mat4.rotate(mMatrix, mMatrix, degToRad(yRot), [0, 1, 0]);
+
+  drawBufferObject(
+    cuboVertexPositionBuffer,
+    null,
+    cuboVertexIndexBuffer,
+    gl.TRIANGLES,
+    cuboVertexTextureCoordBuffer,
+    texturas[0]
+  );
+}
+
+let lastTimestamp = 0;
+function animate() {
+  var currentTimestamp = new Date().getTime();
+
+  if (lastTimestamp != 0) {
+    var diffInSeconds = (currentTimestamp - lastTimestamp) / 1000.0;
+
+    xRot += (xVelo * diffInSeconds) % 360.0;
+    yRot += (yVelo * diffInSeconds) % 360.0;
+  }
+
+  lastTimestamp = currentTimestamp;
+}
+
+function createCubo() {
+  // eslint-disable-next-line no-undef
+  cuboVertexPositionBuffer = createCuboVertexPositionBuffer();
+  // eslint-disable-next-line no-undef
+  cuboVertexIndexBuffer = createCuboVertexIndexBuffer();
+  // eslint-disable-next-line no-undef
+  cuboVertexTextureCoordBuffer = createCuboVertexTextureCoordBuffer();
+}
+
+function iniciarTextura() {
+  var images = new Image();
+
+  for (var i = 0; i < 3; i++) {
+    var textura = gl.createTexture();
+    textura.image = images;
+    texturas.push(textura);
+  }
+
+  images.onload = function () {
+    tratarTextura(texturas);
+  };
+
+  images.src = "caixa.gif";
+  shaderProgram.samplerUniform = gl.getUniformLocation(
+    shaderProgram,
+    "uSampler"
+  );
+}
+
+function tratarTextura(texturas) {
+  gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
+
+  gl.bindTexture(gl.TEXTURE_2D, texturas[0]);
+  gl.texImage2D(
+    gl.TEXTURE_2D,
+    0,
+    gl.RGBA,
+    gl.RGBA,
+    gl.UNSIGNED_BYTE,
+    texturas[0].image
+  );
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
+
+  gl.bindTexture(gl.TEXTURE_2D, texturas[1]);
+  gl.texImage2D(
+    gl.TEXTURE_2D,
+    0,
+    gl.RGBA,
+    gl.RGBA,
+    gl.UNSIGNED_BYTE,
+    texturas[1].image
+  );
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+
+  gl.bindTexture(gl.TEXTURE_2D, texturas[2]);
+  gl.texImage2D(
+    gl.TEXTURE_2D,
+    0,
+    gl.RGBA,
+    gl.RGBA,
+    gl.UNSIGNED_BYTE,
+    texturas[2].image
+  );
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
+  gl.texParameteri(
+    gl.TEXTURE_2D,
+    gl.TEXTURE_MIN_FILTER,
+    gl.LINEAR_MIPMAP_NEAREST
+  );
+
+  gl.generateMipmap(gl.TEXTURE_2D);
+
+  gl.bindTexture(gl.TEXTURE_2D, null);
+}
+
+function eventoTeclaPress(evento) {
+  teclasPressionadas[evento.keyCode] = true;
+}
+
+function eventoTeclaSolta(evento) {
+  teclasPressionadas[evento.keyCode] = false;
+}
