1b4af633c655e4792b045c3050c4c75ff167214d
André Neves <andreneves3@gmail.com>
2021-02-19 21:18:01 -0300

    feat: draw pyramids

--- public/projetos/fase-01/scene/scene.js
+++ public/projetos/fase-01/scene/scene.js
@@ -25,19 +25,45 @@ function Scene() {
   }
 
   function tickDrawSceneObjects() {
-    cube.drawIsolated([1, -1, 1], [0, 0, 0], [100, 1, 100], textures[2], false);
+    // floor
+    cube.drawIsolated(
+      [1, -1, -40],
+      [0, 0, 0],
+      [100, 1, 100],
+      textures[2],
+      false
+    );
 
-    cube.drawIsolated([8, 7, 0], [rotation, 0, 0], [2, 1, 1], textures[0]);
-    cube.drawIsolated([16, 7, 0], [0, rotation, 0], [1, 1, 2], textures[1]);
+    pyramid.drawIsolated([-20, 0, -100], [0, 0, 0], [30, 35, 30], textures[3]);
 
-    pyramid.drawIsolated([-20, 0, 2], [0, 0, 0], [7, 7, 7], textures[1]);
-    pyramid.drawIsolated([-2, 0, 5], [0, 0, 0], [5, 5, 5], textures[3]);
-    pyramid.drawIsolated([-14, 0, -25], [0, 0, 0], [15, 15, 15], textures[0]);
-    // pyramid.drawIsolated([-4, 9, 0], [0, 0, 0], [8, 8, 8], textures[1]);
+    pyramid.drawIsolated([-45, 0, -30], [0, 0, 0], [15, 20, 15], textures[1]);
+    pyramid.drawIsolated([5, 0, -30], [0, 0, 0], [15, 20, 15], textures[4]);
 
-    cube.draw([12, 4, 0], [0, 0, 0], [2, 0.5, 2], textures[0]);
-    cube.drawIsolated([-4, -2, 0], [0, 0, 0], [2, 1, 1], textures[3]);
-    cube.drawIsolated([4, -2, 0], [0, 0, 0], [3, 1, 1], textures[4]);
+    pyramid.drawIsolated([-45, 0, 5], [0, 0, 0], [10, 15, 10], textures[0]);
+    pyramid.drawIsolated([-20, 0, 5], [0, 0, 0], [10, 15, 10], textures[2]);
+    pyramid.drawIsolated([2, 0, 5], [0, 0, 0], [10, 15, 10], textures[3]);
+
+    pyramid.drawIsolated([-30, 0, 30], [0, 0, 0], [7, 9, 7], textures[1]);
+    pyramid.drawIsolated([-7, 0, 30], [0, 0, 0], [7, 9, 7], textures[0]);
+
+    cube.drawIsolated([30, 7, 0], [rotation, 0, 0], [2, 1, 1], textures[0]);
+    cube.drawIsolated([38, 7, 0], [0, rotation, 0], [1, 1, 2], textures[1]);
+
+    cube.draw([34, 4, 0], [0, rotation, 0], [2, 0.5, 2], textures[0]);
+    cube.drawIsolated(
+      [-4, -2, 0],
+      [0, rotation, 0],
+      [1, 1, 1],
+      textures[3],
+      false
+    );
+    cube.drawIsolated(
+      [4, -2, 0],
+      [0, rotation, 0],
+      [1, 1, 1],
+      textures[4],
+      false
+    );
 
     animate();
   }
